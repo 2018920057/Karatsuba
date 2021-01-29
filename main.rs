d7e5ae7ecec0d0b3f24243e6326b6ba47a226c41
20189200571 <zlehfzms@naver.com>
2021-01-29 11:54:20 +0000

    definition of functions

--- main.rs
+++ main.rs
@@ -1,3 +1,21 @@
 fn main() {
-  println!("Hello World!");
+  a : mut [i32] = [1,2,3,5,2,5,6];
+  b : mut [i32] = [3,1,4,5,1,5,9];
+  c : [i32] = karatsuba(&a,&b);
+  println!("a:{:?}, b:{:?}, a*b:{:?}",c);
+}
+
+//a += b*(10^k)
+fn addTo(a : &mut [i32], b : & [i32], k : i32){
+  //TODO
+}
+
+//a -= b 
+fn subFrom(a : &mut [i32], b : & [i32]){
+  //TODO
+}
+
+//a*b
+fn karatsuba(a : & [i32], b : &[i32]) -> [i32]{
+  //TODO
 }
