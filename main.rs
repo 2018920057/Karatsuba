e722eed9c612b596a598b825b2a53f8b3704ff00
20189200571 <zlehfzms@naver.com>
2021-01-29 11:58:48 +0000

    changed arrays to vectors

--- main.rs
+++ main.rs
@@ -1,21 +1,23 @@
 fn main() {
-  a : mut [i32] = [1,2,3,5,2,5,6];
-  b : mut [i32] = [3,1,4,5,1,5,9];
-  c : [i32] = karatsuba(&a,&b);
-  println!("a:{:?}, b:{:?}, a*b:{:?}",c);
+  let mut a : Vec<i32> = vec![1,2,3,5,2,5,6];
+  let mut b : Vec<i32> = vec![3,1,4,5,1,5,9];
+  let c : Vec<i32> = karatsuba(&a,&b);
+  println!("a:{:?}, b:{:?}, a*b:{:?}",a,b,c);
 }
 
 //a += b*(10^k)
-fn addTo(a : &mut [i32], b : & [i32], k : i32){
+fn addTo(a : &mut Vec<i32>, b : & Vec<i32>, k : i32){
   //TODO
 }
 
 //a -= b 
-fn subFrom(a : &mut [i32], b : & [i32]){
+fn subFrom(a : &mut Vec<i32>, b : & Vec<i32>){
   //TODO
 }
 
 //a*b
-fn karatsuba(a : & [i32], b : &[i32]) -> [i32]{
+fn karatsuba(a : & Vec<i32>, b : & Vec<i32>) -> Vec<i32>{
   //TODO
+  let mut ret : Vec<i32> = Vec::new();
+  ret
 }
